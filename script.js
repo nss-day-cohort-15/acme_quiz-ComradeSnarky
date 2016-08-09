0ceb5dcadcec7c51d37dba6abf9cafa7cf9325a1
Lauren Kight <kight6@gmail.com>
2016-08-09 15:07:29 -0500

    added scripts to bootstrap and examples of promises

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("tada")
+
+// // Promise principle #2: Promise `.then`s can be chained
+
+// getJSON('catagories.json')
+//   .then(function () {
+//     console.log('`.then` #1 will fire first')
+//   })
+//   .then(function (catagory) {
+//     console.log('`.then` #2 will fire second')
+//   })
+
+// // The return value of the first then becomes the argument to the second then
+
+// getJSON('types.json')
+//   .then(function (arg) {
+//     console.log("`.then` #1's argument contains the return value of the promise:", arg)
+//     return 'Hello #2 from #1'
+//   })
+//   .then(function (arg) {
+//     console.log("`.then` #2's argument contains the return value of the first `.then`:", arg)
+//   })
+
+// getJSON('products.json')
+//   .then(function (arg) {
+//     console.log("`.then` #1's argument contains the return value of the promise:", arg)
+//     return 'Hello #2 from #1'
+//   })
+//   .then(function (arg) {
+//     console.log("`.then` #2's argument contains the return value of the first `.then`:", arg)
+//   })
