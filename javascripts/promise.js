6ac2da7ca7f4be5d85d6d420b40b560ddae287e7
Lauren Kight <kight6@gmail.com>
2016-08-19 12:12:48 -0500

    fireworks showing on DOM

--- javascripts/promise.js
+++ javascripts/promise.js
@@ -1,127 +1,120 @@
-'use strict';
+"use strict";
 
 
-let row = document.getElementById("row");
-let output = "";
-// This function does one thing, and returns a promise
-var promise1 = getCategories();
-function getCategories() {
-  return new Promise((resolve, reject) => {
-    var catLoader = new XMLHttpRequest();
-     catLoader.open("GET", "jsons/categories.json");
-    catLoader.send();
+$(document).ready(function() {
+  let dropdownMenu = $(".dropdown-menu");
+  var categories=[];
+  var products=[];
+  var types=[];
 
-    catLoader.addEventListener("load", function(){
-      var listOfCategories = JSON.parse(this.responseText).categories;
-      resolve(listOfCategories);
-    });
-    catLoader.addEventListener("error", function(){
-      reject();
-    });
-
-  });
-};
-
-// This function does one thing, and returns a promise
-
-var promise2 = getTypes();
-function getTypes() {
-  return new Promise((resolve, reject) => {
-    var typeLoader = new XMLHttpRequest();
-    typeLoader.open("GET", "jsons/types.json");
-    typeLoader.send();
-
-    typeLoader.addEventListener("load", function(){
-      var listOfTypes = JSON.parse(this.responseText).types;
-      resolve(listOfTypes);
-    });
-    typeLoader.addEventListener("error", function(){
-      reject();
-    });
-
-  });
-};
-
-
-// This function does one thing, and returns a promise
-
-var promise3 = getProducts();
-function getProducts() {
-    return new Promise((resolve,reject) => {
-      var prodLoader = new XMLHttpRequest();
-      prodLoader.open("GET", "jsons/products.json");
-      prodLoader.send();
-
-      prodLoader.addEventListener("load", function(){
-        var listOfProducts = JSON.parse(this.responseText).products;
-        resolve(listOfProducts);
+  var firstXHR = function(){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "jsons/categories.json"
+      }).done(function(data){
+        resolve(data);
+      }).fail(function(xhr, status, error) {
+        reject(error);
       });
-      prodLoader.addEventListener("error", function(){
-        reject();
-      });
-
     });
   };
 
-
-//use above Promises to describe the order of execution,
-promise1.then(function(data1){
-  console.log('data', data1)
-});
-promise2.then(function(data2) {
-  console.log('data', data2)
-});
-promise3.then(function(data3) {
-  console.log('data', data3)
-});
-
-  // show in DOM
-function buildCard(products) {
-  for (var i = 0; i < products.length; i++) {
-  row.innerHTML += `<div class="row">${products[i].name.value}</div><div>${products[i].categories.value}/div>`;
+  var secondXHR = function(result_of_firstXHR){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "jsons/types.json",
+        // data: result_of_firstXHR
+      }).done(function(data){
+        resolve(data);
+      }).fail(function(xhr, status, error){
+        reject(error);
+      });
+    });
   };
-};
-buildCard();
+
+  var thirdXHR = function(result_of_secondXHR){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "jsons/products.json",
+        // data: result_of_secondXHR
+      }).done(function(data){
+        resolve(data);
+      }).fail(function(xhr, status, error){
+        reject(error);
+      });
+    });
+  };
+
+  firstXHR()
+    .then(function(data1){
+      categories = data1.categories;
+      populateDropdown(categories);
+      return secondXHR(data1);
+    })
+    .then(function(data2){
+      types = data2.types;
+      return thirdXHR(data2);
+    }).then(function(data3){
+      let products1 = data3.products;
+      products1.forEach(function(product){
+        products.push(product[Object.keys(product)[0]]);
+      });
+      organizeAcme();
+    });
+
+  function populateDropdown(categories){
+    dropdownMenu.html("");
+    categories.forEach(function(cats){
+      dropdownMenu.append(`<option value="${cats.id}">${cats.name}</option>`);
+    });
+  }
+
+  function populateDOM(acmeArray){
+    let acme = $("#container-fluid");
+    acme.html("");
+    acmeArray.forEach(function(product){
+      $(`<div class = "col-md-4 col-md-4"><p>${product.name} - ${product.description}</p></div>`).appendTo(acme);
+    });
+  }
+
+  function organizeAcme(){
+    let finalProducts = products;
+    finalProducts.forEach(function(finalProduct){
+      finalProduct.typeInfo = types[finalProduct.type];
+      finalProduct.categoryInfo = categories[finalProduct.typeInfo.category];
+      // console.log("categoryInfo", );
+
+    });
+    // console.log("finalProducts", finalProducts);
+    populateDOM(finalProducts);
+  }
+
+});
 
 
-// var output = document.querySelector(".output")
-//   var results = "" //need variable to build string and push into innerHTML so it does not get closed automatically
-// // Loop over the inventory and populate the page
-//   inventory.forEach(function(car, i){ //argument in function(car) becomes the 'i' like in a regular for loop
-//     if (i % 3 === 0) { // starts a new row at the point to which your cards go to the next row (3 can be however many 'cards' you will have on each row)
-//       results += `<div class="row">`
-//     }
-//     results += `
-      // <div class="col-md-4 carCard" style="border-color: ${car.color}">
-      // <h3>${car.make}</h3>
-      // <h3>${car.model}</h3>
-      // <h3>${car.year}</h3>
-      // <h3>${car.price}</h3>
-      // <h3>${car.color}</h3>
-      // <h3>${car.purchased}</h3>
-      // <p>${car.description}</p>
-      // </div>
-      // ` //+= because each time you are reassigning results //need variable to build string and push into innerHTML so it does not get closed automatically
 
-  // if ((i + 1) % 3 === 0) {
-  //   results += `</div>`
-  // }
-  // })
-  // output.innerHTML = results
+// function catDropdown(data1) {
+//   data1.forEach(function(category) {
+//     $('<option />', {
+//       value: category.id,
+//       text: category.name
+//     }).appendTo(categoryDrop);
+//     })
+//   }
+//   function typeDropdown(data2) {
+//     data2.forEach(function(type) {
+//       $('<option />', {
+//         value: type.id,
+//         text: type.name,
+//         description: type.description
 
-// var loadInventory = function () { //loadInventory is a function that immediately returns a promised object
-//       return new Promise(function (resolve, reject) {
-//       var inventoryLoader = new XMLHttpRequest();
-//       inventoryLoader.open('GET', 'inventory.json')
-//       inventoryLoader.send();
+//       }).appendTo(typeDrop);
+// })
+// }
 
-//       inventoryLoader.addEventListener("load", function () {
-//         var data = JSON.parse(this.responseText)
-//         fillInventory(data);
-//         resolve(inventory); //no longer responsible for calling populatePage
-//       });
-//      });
-//     };
+
+
+
 
 //shorthand
 // module.exports = {getCategories, getTypes, getProducts};
